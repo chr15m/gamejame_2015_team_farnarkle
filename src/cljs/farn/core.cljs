6cee2b0c04894fdab09017868cd7ec186619caf8
Crispin Wellington <retrogradeorbit@gmail.com>
2015-01-23 23:40:32 +0800

    Rotating goodness FTW

--- src/cljs/farn/core.cljs
+++ src/cljs/farn/core.cljs
@@ -68,12 +68,26 @@
               ["http://fonts.gstatic.com/s/lobster/v12/cycBf3mfbGkh66G5NhszPQ.woff2"
                "http://fonts.gstatic.com/s/varelaround/v6/APH4jr0uSos5wiut5cpjrugdm0LZdjqr5-oayXSOefg.woff2"
                "http://fonts.gstatic.com/s/shadowsintolighttwo/v4/gDxHeefcXIo-lOuZFCn2xbIfIbZrEcH2WW587lG91II.woff2"
+               "img/sprites/pink-stand-1.png"
+               "img/sprites/pink-stand-2.png"
+               "img/sprites/pink-stand-3.png"
+               "img/sprites/pink-stand-4.png"
+               "img/sprites/static-tree-1.png"
+               "img/sprites/static-tree-2.png"
+               "img/sprites/static-tree-3.png"
+               "img/sprites/static-tree-4.png"
+               "img/sprites/static-tree-5.png"
+               "img/sprites/static-tree-6.png"
+               "img/sprites/static-tree-7.png"
+               "img/sprites/static-tree-8.png"
+               "img/sprites/static-tree-9.png"
+               "img/sprites/static-tree-10.png"
                ]
               :full-colour 0x306020
               :highlight 0x80ff80
               :lowlight 0x103010
               :empty-colour 0x000000
-              :debug-delay 1.0
+              :debug-delay 0.1
               :fade-in 0.2
               :fade-out 2)]
     (log "pulling")
@@ -82,11 +96,78 @@
     (<! (timeout 2000))
     (let [
           lobster-big (font/make-tiled-font "Lobster" 400 40)
-          title-text (font/font-make-batch lobster-big "Alien Forest Explorer" )]
-      (.addChild ui-stage (:sprite title-text))
-      (<! (timeout 5000))
-      ;(.removeChild ui-stage (:sprite title-text))
-)))
+          title-text (font/font-make-batch lobster-big "Alien Forest Explorer" )
+          guy (gfx/make-sprite (js/PIXI.Texture.fromImage "img/sprites/pink-stand-1.png"))
+          trees (for [i (range 50)]
+                  (js/PIXI.Texture.fromImage (str "img/sprites/static-tree-" i ".png")))
+          tufts (for [i (range 3)]
+                  (js/PIXI.Texture.fromImage (str "img/sprites/static-tuft-" i ".png")))
+
+          depth-compare (fn [a b]
+                          ;(log "comp" (.-position.y a) (.-position.y b))
+                          (cond
+                           (< (.-position.y a) (.-position.y b)) -1
+                           (< (.-position.y b) (.-position.y a)) 1
+                           :default 0))]
+                                        ;(.addChild ui-stage (:sprite title-text))
+      (<! (timeout 1000))
+      (log "adding")
+      (doto guy
+        (sprite/set-scale! 0.5))
+      (.addChild main-stage guy)
+
+      (doseq [t-num (range 100)]
+        (let [tree (gfx/make-sprite (rand-nth trees))
+              width (.-width tree)
+              height (.-height tree)]
+          (log "!" tree)
+          (doto tree
+            (sprite/set-scale! 0.5)
+            (sprite/set-pivot! (/ width 2) height)
+            (sprite/set-pos!
+             ;;0 0
+              (- (rand 1000) 500)
+              (- (rand 1000) 500)
+             ))
+          (.addChild main-stage tree)))
+
+      (doseq [t-num (range 100)]
+        (let [tufts (gfx/make-sprite (rand-nth tufts))
+              width (.-width tufts)
+              height (.-height tufts)
+              ]
+          (log "!" tufts)
+          (doto tufts
+            (sprite/set-scale! 0.5)
+            (sprite/set-pivot! (/ width 2) height)
+            (sprite/set-pos! (- (rand 1000) 500) (- (rand 1000) 500)))
+          (.addChild main-stage tufts)))
+
+      (.sort (.-children main-stage) depth-compare)
+
+      (loop []
+        (let [d-theta 0.01
+              xc (Math/cos d-theta)
+              yc (Math/sin d-theta)]
+          (log xc yc)
+          (doseq [i (range 200)]
+            (let [sp (aget (.-children main-stage) i)]
+              (sprite/set-pos!
+               sp
+               (-
+                (* xc (.-position.x sp))
+                (* yc (.-position.y sp)))
+               (+
+                (* yc (.-position.x sp))
+                (* xc (.-position.y sp))))
+              ))
+          )
+        (.sort (.-children main-stage) depth-compare)
+        (<! (events/next-frame))
+        (recur))
+
+                                        ;(.removeChild ui-stage (:sprite title-text))
+      )))
 
 ;; (def lobster-big (font/make-tiled-font "Lobster" 400 40))
 
