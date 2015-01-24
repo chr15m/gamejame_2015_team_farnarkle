8a378e1cbf4e26e96de476b36be45ef63d349b7c
Crispin Wellington <retrogradeorbit@gmail.com>
2015-01-24 12:04:18 +0800

    new spatial hash code

---
+++ src/cljs/farn/spatial.cljs
@@ -0,0 +1,34 @@
+(ns farn.spatial
+  (:require [dommy.core :as dommy :refer-macros [sel1]]
+            [cljs.core.async :refer [put! chan <! >! alts! timeout]]
+            [farn.utils :refer [log ends-with? url-keyword rand-between]])
+  (:require-macros [cljs.core.async.macros :refer [go]])
+)
+
+(defn make-random-map
+  "make a massive crappy map. obj-keys is a list of texture keywords.
+  number is how many will be spread out across the map
+  max/mins are extents
+  "
+  [obj-keys number minx maxx miny maxy]
+  (doall (for [i (range number)]
+           {:pos [(rand-between minx maxx)
+                  (rand-between miny maxy)]
+            :type (rand-nth obj-keys)})))
+
+(defn hash-locations
+  "return a spatially hashed tree of all the sprites in collection, based apon :pos key"
+  [collection cell-size]
+  (loop [output {}
+         [h & t] collection]
+    (log (str output) )
+    (let [[x y] (:pos h)
+          xh (int (/ x cell-size))
+          yh (int (/ y cell-size))
+          key [xh yh]]
+      (if (empty? t)
+        output
+        (if (contains? output key)
+          (recur (assoc output key (conj (output key) h)) t)
+          (recur (assoc output key [h]) t))
+        ))))
