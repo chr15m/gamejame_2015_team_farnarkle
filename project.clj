0cdb3c540bbdaa5b588721c1340c8a0f0e40dd17
Crispin Wellington <retrogradeorbit@gmail.com>
2015-01-24 15:40:22 +0800

    clojurescript bump

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :source-paths ["src/clj" "src/cljs"]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2371" :scope "provided"]
+                 [org.clojure/clojurescript "0.0-2511" :scope "provided"]
                  [ring "1.3.1"]
                  [compojure "1.2.0"]
                  [enlive "1.1.5"]
