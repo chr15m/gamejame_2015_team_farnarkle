503b45dab5c49f8c247b3ef9e20edebcfc3f8524
Chris McCormick <chris@mccormick.cx>
2015-01-25 15:15:23 +0800

    Deploy tweaks.

--- bin/push-to-gh-pages.sh
+++ bin/push-to-gh-pages.sh
@@ -3,6 +3,7 @@
 # get into this script's own root
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
+ALLFILES=index.html css img js music sfx
 OUTDIR=gh-pages-build/
 COMMIT_ID=`git rev-parse master`
 
@@ -12,15 +13,16 @@ echo "Building statically with lein cljsbuild."
 # lein with-profile -dev,+build-static cljsbuild once
 lein with-profile +dev cljsbuild once
 rm -rf gh-pages-build
+rm -rf $ALLFILES
 mkdir -p gh-pages-build && \
 rsync -a --progress resources/public/ $OUTDIR && \
 echo "Running 'lein dump-index-html'"
 lein dump-index-html > $OUTDIR/index.html
 echo "Checking out gh-pages branch and syncing."
 git checkout gh-pages && \
-rsync -a --progress $OUTDIR .
-echo "Adding everything to the gh-pages branch."
-git add css img index.html js music sfx
+rsync -a --progress $OUTDIR . && \
+echo "Adding everything to the gh-pages branch." && \
+git add $ALLFILES
 echo "Now do:"
 echo "git commit -m 'gh-pages build of commit ID $COMMIT_ID'"
 echo "git push # to get gh-pages branch live."
